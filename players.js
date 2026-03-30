f6bc5f2a0995ed4c19e8a1854817ea583eb896ff
RellixsMF <rellixsouja@email.com>
2026-03-30 20:21:33 +0500

    shadow

---
+++ players.js
@@ -0,0 +1,451 @@
+// ================================================================
+// UZTIERS — Global Players Database
+// Admin panel orqali boshqariladi
+// ================================================================
+
+window.UZTIERS_PLAYERS = [
+  {
+    name: "UmarBlade",
+    points: 420,
+    rank: "Combat Grandmaster",
+    avatar: "https://render.crafty.gg/3d/bust/63ee4a6f-1194-49f6-a89a-669af5387751",
+    region: "AS",
+    tiers: [
+      { icon: "sword",   label: "HT1" },
+      { icon: "nethop",  label: "HT1" },
+      { icon: "vanilla", label: "HT1" },
+      { icon: "axe",     label: "HT1" },
+      { icon: "uhc",     label: "HT1" },
+      { icon: "mace",    label: "HT3"},
+      { icon: "pot",     label: "HT1" }
+    ]
+  },
+  {
+    name: "VyoxYouTube",
+    points: 26,
+    rank: "Combat Ace",
+    avatar: "https://mytiers.ru/api/avatar/VyoxYouTube",
+    region: "AS",
+    tiers: [
+      { icon: "sword",  label: "LT2" },
+      { icon: "nethop", label: "LT3" }
+    ]
+  },
+  {
+    name: "Condonings",
+    points: 135,
+    rank: "Combat Ustasi",
+    avatar: "https://www.mytiers.ru/api/avatar/Condonings",
+    region: "AS",
+    tiers: [
+      { icon: "sword",   label: "LT1" },
+      { icon: "nethop",  label: "LT1" },
+      { icon: "vanilla", label: "LT1" }
+    ]
+  },
+  {
+    name: "ByronFrank",
+    points: 154,
+    rank: "Combat Ustasi",
+    avatar: "https://render.crafty.gg/3d/bust/b01d1c8d-4c5d-4a4f-9592-fd082bd8f5b1",
+    region: "AS",
+    tiers: [
+      { icon: "sword",   label: "RLT3" },
+      { icon: "axe",     label: "RLT2" },
+      { icon: "uhc",     label: "RHT4" },
+      { icon: "mace",    label: "RLT1" },
+      { icon: "vanilla", label: "RHT4" },
+      { icon: "nethop",  label: "RLT1" },
+      { icon: "smp",     label: "RHT2" }
+    ]
+  },
+  {
+    name: "Sharpvex",
+    points: 98,
+    rank: "Combat Ustasi",
+    avatar: "https://render.crafty.gg/3d/bust/d62ae6ed-7835-4de0-8779-fac212ddac94",
+    region: "AS",
+    tiers: [
+      { icon: "sword",   label: "HT2" },
+      { icon: "nethop",  label: "LT3" },
+      { icon: "smp",     label: "LT3" },
+      { icon: "mace",    label: "LT3" },
+      { icon: "axe",     label: "HT3" },
+      { icon: "uhc",     label: "LT5" },
+      { icon: "vanilla", label: "HT4" }
+    ]
+  },
+  {
+    name: "Xolodilda",
+    points: 60,
+    rank: "Combat Ace",
+    avatar: "https://render.crafty.gg/3d/bust/fa7e8baa-4846-440b-85aa-8c5df6e53db6",
+    region: "AS",
+    tiers: [
+      { icon: "vanilla", label: "HT1" }
+    ]
+  },
+  {
+    name: "Bekojon",
+    points: 64,
+    rank: "Combat Ace",
+    avatar: "https://render.crafty.gg/3d/bust/60d552ba-e915-415d-b469-06e0b988c4d1",
+    region: "AS",
+    tiers: [
+      { icon: "mace",  label: "HT1" },
+      { icon: "sword", label: "HT4" }
+    ]
+  },
+  {
+    name: "KamronCC",
+    points: 36,
+    rank: "Combat Ace",
+    avatar: "Screenshot 2026-03-20 194238.png",
+    region: "AS",
+    tiers: [
+      { icon: "vanilla", label: "HT2" },
+      { icon: "sword",   label: "LT4" },
+      { icon: "mace",    label: "LT4" }
+    ]
+  },
+  {
+    name: "ShadowBlaydz",
+    points: 16,
+    rank: "Combat Specialist",
+    avatar: "Screenshot 2026-03-21 133843.png",
+    region: "AS",
+    tiers: [
+      { icon: "vanilla", label: "HT5" },
+      { icon: "sword",   label: "HT3" },
+      { icon: "mace",    label: "HT5" },
+      { icon: "pot",     label: "HT5" }
+    ]
+  },
+  {
+    name: "Infural",
+    points: 20,
+    rank: "Combat Ace",
+    avatar: "https://mytiers.ru/api/avatar/192Tenzo",
+    region: "AS",
+    tiers: [
+      { icon: "mace", label: "LT2" }
+    ]
+  },
+  {
+    name: "ALI_GAMING_YT",
+    points: 9,
+    rank: "Combat Specialist",
+    avatar: "photo_2026-03-21_19-26-33.jpg",
+    region: "AS",
+    tiers: [
+      { icon: "mace",  label: "HT4" },
+      { icon: "sword", label: "HT4" },
+      { icon: "vanilla",   label: "LT5" }
+    ]
+  },
+  {
+    name: "PrimeRealXvFpvp",
+    points: 4,
+    rank: "Combat Cadet",
+    avatar: "photo_2026-03-21_19-23-10.jpg",
+    region: "AS",
+    tiers: [
+      { icon: "mace",    label: "HT5" },
+      { icon: "vanilla", label: "LT5" },
+      { icon: "sword",   label: "LT5" }
+    ]
+  },
+  {
+    name: "Carlojon",
+    points: 206,
+    rank: "Combat Ustasi",
+    avatar: "Screenshot 2026-03-21 220932.png",
+    region: "AS",
+    tiers: [
+      { icon: "sword", label: "LT1" },
+      { icon: "uhc",  label: "LT1" },
+      { icon: "vanilla", label: "LT2" },
+      { icon: "mace", label: "LT3" },
+      { icon: "nethop", label: "LT1" },
+      { icon: "pot", label: "LT1" }
+      
+    ]
+  },
+  {
+    name: " ClownPsycho",
+    points: 38,
+    rank: "Combat Ace",
+    avatar: "https://mytiers.ru/api/avatar/Wanda07",
+    region: "AS",
+    tiers: [
+      { icon: "sword", label: "HT5" },
+      { icon: "nethop",  label: "HT2" },
+      { icon:"uhc", label:"HT4"},
+      { icon: "mace",    label: "HT5" }
+
+    ]
+  },
+   {
+  name: "ItzRaket",
+    points: 14,
+    rank: "Combat Specialist",
+    avatar: "Screenshot 2026-03-22 125725.png",
+    region: "AS",
+    tiers: [
+      { icon: "sword", label: "HT4" },
+      { icon:"uhc", label:"HT4"},
+      { icon: "mace",    label: "LT3" }
+
+    ]
+   },
+   {
+  name: "itzRellixsMF",
+    points: 26,
+    rank: "Combat Ace",
+    avatar: "https://mytiers.ru/api/avatar/oleg_321f",
+    region: "AS",
+    tiers: [
+      { icon: "sword", label: "LT4" },
+      { icon:"uhc", label:"LT5"},
+      { icon: "mace",    label: "LT4" },
+      {icon:"vanilla", label:"LT5"},
+      {icon:"axe", label:"LT4"},
+      {icon:"pot", label:"LT4"}
+
+    ]
+   },
+   {
+   name: " 1Ferd",
+    points: 9,
+    rank: "Combat Specialist",
+    avatar: "https://mytiers.ru/api/avatar/VyoxYouTube",
+    region: "AS",
+    tiers: [
+      {icon:"vanilla", label:"HT4"},
+      {icon:"uhc", label:"HT5"},
+      {icon:"sword", label:"LT5"},
+      {icon:"mace", label:"LT4"}
+    ]
+   },
+   {
+   name: "Ustoz_Ugve",
+    points: 4,
+    rank: "Combat Cadet",
+    avatar: "https://mytiers.ru/api/avatar/oleg_321f",
+    region: "AS",
+    tiers: [
+      {icon:"sword", label:"HT4"},
+      
+    ]
+   },
+   {
+   name: "Fozilbek1",
+    points: 34,
+    rank: "Combat Ace",
+    avatar: "https://render.crafty.gg/3d/bust/e49bc1f3-ca8a-4ba3-a06a-4845625fdfb4",
+    region: "AS",
+    tiers: [
+      {icon:"mace", label:"LT2"},
+      {icon:"vanilla", label:"HT5"},
+      {icon:"sword", label:"HT5"},
+      {icon:"smp", label:"LT3"},
+    ]
+   },
+   {
+    name: "LITEKINGS_12",
+      points: 5,
+      rank: "Combat Cadet",
+      avatar: "https://render.crafty.gg/3d/bust/d2be01d3-f423-421b-be4d-cb72f2a0ae47",
+      region: "AS",
+      tiers: [
+        {icon:"mace", label:"HT5"},
+        {icon:"vanilla", label:"LT5"},
+        {icon:"pot", label:"HT5"},
+      ]
+   },
+    {
+    name: "ItzBabur",
+      points: 130,
+      rank: "Combat Ustasi",
+      avatar: "Screenshot 2026-03-23 133834.png",
+      region: "AS",
+      tiers: [
+        {icon:"sword", label:"LT2"},
+        {icon:"uhc", label:"LT1"},
+        {icon:"nethop", label:"LT1"},
+        {icon:"mace", label:"LT2"}
+      ]
+   },
+   {
+    name: "ya_ne_love4k",
+      points: 9,
+      rank: "Combat Specialist",
+      avatar: "https://mytiers.ru/api/avatar/oleg_321f",
+      region: "AS",
+      tiers: [
+        {icon:"sword", label:"LT3"},
+        {icon:"uhc", label:"LT5"},
+        {icon:"mace", label:"HT5"},
+      ]
+   },
+   {
+    name: "davlatbek_pm",
+      points: 6,
+      rank: "Combat Cadet",
+      avatar: "https://mytiers.ru/api/avatar/oleg_321f",
+      region: "AS",
+      tiers: [
+        {icon:"vanilla", label:"LT3"},
+      ]
+   },
+    {
+      name: " Astrokeyv",
+        points: 20,
+        rank: "Combat Ace",
+        avatar: "https://mytiers.ru/api/avatar/erranting",
+        region: "AS",
+        tiers: [
+          {icon:"nethop", label:"LT2"},
+          {icon:"sword", label:"LT2"}
+        ]
+    },
+    {
+      name: "ZintrexYT",
+        points: 2,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/c1nto4k",
+        region: "AS",
+        tiers: [
+          {icon:"vanilla", label:"LT5"},
+          {icon:"sword", label:"LT5"}
+        ]
+    },
+    {
+      name: "Qorbobo_001",
+        points: 3,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/zYeezy",
+        region: "AS",
+        tiers: [
+          {icon:"vanilla", label:"LT4"},
+        ]
+    },
+    {
+      name: "FinicMinecraft",
+        points: 3,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/IXNS",
+        region: "AS",
+        tiers: [
+          {icon:"vanilla", label:"LT5"},
+          {icon:"mace", label:"HT5"}
+        ]
+    },
+      {
+        name: "Mohriddin_1",
+          points: 13,
+          rank: "Combat Cadet",
+          avatar: "https://mytiers.ru/api/avatar/viviennegod",
+          region: "AS",
+          tiers: [
+            {icon:"sword", label:"LT4"},
+            {icon:"vanilla", label:"HT3"},
+          ]
+      },
+      {
+        name: "Tanjiro_pvp",
+          points: 2,
+          rank: "Combat Cadet",
+          avatar: "https://mytiers.ru/api/avatar/teamentr",
+          region: "AS",
+          tiers: [
+            {icon:"mace", label:"HT5"},
+          ]
+      },
+      {
+        name: "grazajon",
+          points: 2,
+          rank: "Combat Cadet",
+          avatar: "https://mytiers.ru/api/avatar/ARTEMM77",
+          region: "AS",
+          tiers: [
+            {icon:"sword", label:"HT5"},
+          ]
+      },
+      {
+        name: "FYZEN_MC",
+          points: 1,
+          rank: "Combat Cadet",
+          avatar: "Screenshot 2026-03-24 230916.png",
+          region: "AS",
+          tiers: [
+            {icon:"sword", label:"LT5"},
+          ]
+      },
+      {
+        name: "PRBLM",
+          points: 2,
+          rank: "Combat Cadet",
+          avatar: "https://mytiers.ru/api/avatar/yonibghx",
+          region: "AS",
+          tiers: [
+            {icon:"sword", label:"HT5"},
+          ]
+      },
+      {
+        name: "ISAGI203",
+          points: 3,
+          rank: "Combat Cadet",
+          avatar: "https://mytiers.ru/api/avatar/zlarrk",
+          region: "AS",
+          tiers: [
+            {icon:"sword", label:"LT4"},
+          ]
+      },
+    {
+      name: " Dezzijan",
+        points: 1,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/dema11031103",
+        region: "AS",
+        tiers: [
+          {icon:"sword", label:"LT5"},
+        ]
+     },
+     {
+      name: "BORZIJON1",
+        points:3,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/1tz_balance",
+        region: "AS",
+        tiers: [
+          {icon:"mace", label:"HT5"},
+          {icon:"sword", label:"LT5"},
+        ]
+     },
+     {
+      name: "Mahmudov_707",
+        points: 6,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/sotxweq",
+        region: "AS",
+        tiers: [
+          {icon:"sword", label:"LT3"},
+        ]
+     },
+     {
+      name: "Dostonbek_2008",
+        points: 1,
+        rank: "Combat Cadet",
+        avatar: "https://mytiers.ru/api/avatar/1tz_balance",
+        region: "AS",
+        tiers: [
+          {icon:"sword", label:"LT5"},
+        ]
+     }
+
+
+   
+   
+
+];
